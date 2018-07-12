7f6899cd5e285cbed7238c7c4b27bd1109937def
Abhinav Priyadarshi <abhinav@zopper.com>
2018-07-12 09:16:28 +0530

    flask blog app

---
+++ Readme.md
@@ -0,0 +1,19 @@
+This is a basic blog
+made in Flask ,mysql, bootstrap,jquery
+users can create an account and add articles ,edit articles or delete the articles
+All the users can see the articles too
+the jinja templating engine is used to render the Html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles=[
+	{
+	  'id':1,
+	  'title':'Article One',
+	  'body':"lorem ipsum dolor sit amet",
+	  'author':'Brad babu',
+	  'create-date':'04-25-2017'
+	},
+	{
+	  'id':2,
+	  'title':'Article Two',
+	  'body':"lorem ipsum dolor sit amet",
+	  'author':'Brad babu',
+	  'create-date':'04-25-2017'
+	},
+	{
+	  'id':3,
+	  'title':'Article Three',
+	  'body':"lorem ipsum dolor sit amet",
+	  'author':'Brad babu',
+	  'create-date':'04-25-2017'
+	}
+	]
+	return articles
